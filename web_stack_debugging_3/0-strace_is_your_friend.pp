8d5ce194702fb832d0585f674b7776db846de1ec
Kiguru-Alu <b.mahui@alustudent.com>
2025-11-25 02:21:41 -0800

    still trying to fix issue

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,13 +1,18 @@
 # Fix the WordPress installation causing Apache to return 500 errors
 
-exec { 'fix-wordpress':
-  command => '/bin/sed -i "s/;$/;/" /var/www/html/wp-config.php',
-  onlyif  => '/bin/grep -q "DB_PASSWORD" /var/www/html/wp-config.php',
+package { 'apache2':
+  ensure => installed,
+}
+
+exec { 'restore-wp-settings':
+  command => 'cp /var/www/html/wp-settings.php.save /var/www/html/wp-settings.php',
+  refreshonly => false,
 }
 
 service { 'apache2':
-  ensure    => running,
-  enable    => true,
-  subscribe => Exec['fix-wordpress'],
+  ensure  => running,
+  enable  => true,
+  require => Package['apache2'],
+  subscribe => Exec['restore-wp-settings'],
 }
 
